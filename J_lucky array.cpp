238605c73ac1b162801a1e9c67fb4c36faabeb32
Nazeha mosharaf <nazehamosharaf@gmail.com>
2025-09-26 10:57:03 +0600

    Update J_lucky array.cpp

--- J_lucky array.cpp
+++ J_lucky array.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 int main()
 {
-    int a,temp,result;
-    cin>>a;
-    for(int i=0;i<a;i++){
-        bool for_testing=true;
-        long long int b;
-        cin>>b;
-        int c[b];
-        for(int i=0;i<b;i++){
-            cin>>c[i];
-        }
-        for(int i=0;i<b;i++){
-            for(int j=i+1;j<b;j++){
-                temp=(c[i]+c[j]+(j+1)-(i+1));
-                if(for_testing==true){
-                    result=temp;
-                    for_testing=false;
-                }
-                if(temp<result){
-                    result=temp;
-                }
-            }
-        }
-        cout<<result<<endl;
-    }
-    return 0;
+int n,i,lowest,p=0;
+cin>>n;
+int a[n];
+for (i=1;i<=n;i++)
+cin>>a[i];
+//finding the lowestv element of the array
+lowest=a[1];
+for (i=2;i<=n;i++)
+{
+if(a[i]<lowest)
+lowest=a[i];
+}
+//finding the lowest elements frequency or value
+for (i=1;i<=n;i++)
+{
+if(lowest==a[i])
+p++;
+}
+//finding the array is lucky or not
+if(p%2!=0)
+{
+cout<<"Lucky";
+}
+else
+cout<<"Unlucky";
 }
